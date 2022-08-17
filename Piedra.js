e2556e02fbc1ee145f1f703315357c177382e2da
CesarCamachoUPT <111400454+CesarCamachoUPT@users.noreply.github.com>
2022-08-16 20:01:19 -0500

    Juego Buscando la Piedra

---
+++ Piedra.js
@@ -0,0 +1,19 @@
+function posiciones(){
+    n["pos1" , "pos2" , "pos3"];
+
+}
+
+function buscarPiedra(){
+    piedra = prompt("Cuantas Vueltas Le daremos:")
+
+    for(let a=0; a<=piedra; a++){
+        for(let b=0; b<=n; b++){
+            n(b);
+        }
+    }
+}
+
+function encontrarPiedra(){
+
+    console.log("")
+}
